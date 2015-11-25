38be6a9c0b46cdc191451eb26d06d0c4a1e62d33
Kenneth Endfinger <kaendfinger@gmail.com>
2015-11-25 11:09:22 -0500

    Remove unused import.

--- lib/msgpack.dart
+++ lib/msgpack.dart
@@ -3,8 +3,6 @@ library msgpack;
 import "dart:convert";
 import "dart:typed_data";
 
-import "package:typed_data/typed_data.dart";
-
 part "src/packer.dart";
 part "src/unpacker.dart";
 part "src/message.dart";
